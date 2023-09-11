ddd6cd998072d1482c6e52940de5dae23e4b39ef
Alexey Kostromin <kostromin.aleksei@gmail.com>
2023-09-11 22:02:52 +0200

    Init commit

---
+++ src/test/java/HomeWork_03.java
@@ -0,0 +1,13 @@
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+public class HomeWork_03 {
+
+    @Test
+    void fillAllForm() {
+        Selenide.open();
+
+
+    }
+
+}
