76a43f8e5e376240c063a4057833997e4bf4940b
Alexey Kostromin <kostromin.aleksei@gmail.com>
2023-09-26 15:16:32 +0200

    wikiPageTest() done

--- src/test/java/SelenideWikiTest.java
+++ src/test/java/SelenideWikiTest.java
@@ -3,8 +3,9 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
 
 public class SelenideWikiTest {
     @BeforeAll
@@ -18,9 +19,12 @@ public class SelenideWikiTest {
         open("https://github.com/selenide/selenide");
         $("[id=wiki-tab]").click();
         $("[id=wiki-body]").shouldHave(Condition.text("Soft assertions"));
-        $("[id=wiki-tab]");
-
-
+        $(byText("Soft assertions")).click();
+        $("[id=user-content-3-using-junit5-extend-test-class]").scrollTo();
+        $("[id=user-content-3-using-junit5-extend-test-class]").shouldHave(text("3. Using JUnit5 extend test class:"));
+        //$("[id=user-content-3-using-junit5-extend-test-class]").sibling(0).shouldHave(text("SoftAssertsExtension"));
+        $x("//*[contains(@id, 'using-junit5-extend')]/following-sibling::div[1]").shouldHave(text("SoftAssertsExtension"));
+        sleep(5000);
     }
 
 }
